c0dfb036f88e723b50a47648689817cdc31f088d
12345 <daniel880830@gmail.com>
2019-04-25 20:56:40 +0800

    ok

--- app/src/main/java/com/example/edittext/MainActivity.java
+++ app/src/main/java/com/example/edittext/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setBackgroundDrawableResource(R.drawable.hedgehog);
     }
-    int size=30;
+    int size=15;
     public void response(View v){
         EditText ed1,ed2,ed3;
         TextView txv;
@@ -24,7 +26,33 @@ public class MainActivity extends AppCompatActivity {
         ed3=findViewById(R.id.editText3);
         txv=findViewById(R.id.textView);
 
+        float height = Float.parseFloat(ed2.getText().toString());
+        float weight = Float.parseFloat(ed3.getText().toString());
+        float BMI1 = calculateBMI(height,weight);
+        String BMI2 = String.valueOf(BMI1);
+
         txv.setTextSize(size);
-        txv.setText(ed1.getText()+"您好\n"+"您的BMI是:");
+        txv.setText(ed1.getText().toString()+"Hello"+",n" +"your BMI is"+BMI2);
+        Toast.makeText( this, standardBMI(BMI1), Toast.LENGTH_LONG).show();
+    }
+
+    private float calculateBMI(float ht, float wt){
+        float BMI = (float)(wt/ Math.pow((ht / 100),2));
+        return BMI;
+    }
+
+    private String standardBMI(double st){
+        String alert = " ";
+
+        if(st>25){
+            alert = String.valueOf("You are too heavy!!!");
+            getWindow().setBackgroundDrawableResource(R.drawable.catc);
+        }else if(st<18.5){
+            alert = String.valueOf("You are too slim.");
+            getWindow().setBackgroundDrawableResource(R.drawable.catn);
+        }else{
+            alert = String.valueOf("Your body is good");
+        }
+        return alert;
     }
 }
